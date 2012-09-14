d8044334d3ef53db0e81d426f989ca539bcbba2e
James Higgs <james7780@yahoo.com>
2012-09-14 11:57:29 +0200

    added socket.io

--- server.js
+++ server.js
@@ -18,7 +18,9 @@
 
 var PORT = 8765;
 
-var net = require('net');
+//var net = require('net');
+var io = require('socket.io').listen(8765);					// have to have "npm install socket.io" first
+
 var galaxyjs = require('./galaxy.js');				// the game world
 
 var sockets = [];
@@ -30,21 +32,30 @@ function socketData(data) {
 
 // Handles new connections
 function serverListener(socket) { //'connection' listener
-  console.log('server connected on port' + socket.address().port);
+  //console.log('server connected on port' + socket.options.port);
 
-  sockets.push(socket);
+  console.log('server connected: ' + socket.id);
+
+  //sockets.push(socket);
+
+  socket.emit('hello\r\n');
+  //socket.pipe(c);
+
 
   socket.on('end', function() {
     console.log('server disconnected');
   });
-  socket.write('hello\r\n');
-  //socket.pipe(c);
 
   socket.on('data', socketData);
   
   socket.on('state', function() {
-    console.log('state request from ' + socket.address().port);		// externaladdress?
+    console.log('state request from ' + socket.id);		// externaladdress?
     socket.emit('state', "abc");
+    });
+
+  socket.on('join', function() {
+    console.log('join request from ' + socket.id);		// externaladdress?
+    socket.emit('join', "abc");
     });
 
 	// Test we can get game world started
@@ -53,6 +64,8 @@ function serverListener(socket) { //'connection' listener
 	game.initialise(1);
 };
 
-var server = net.createServer(serverListener);
+//var server = net.createServer(serverListener);
+//server.listen(PORT);
 
-server.listen(PORT);
+io.sockets.on('connection', serverListener);
+
